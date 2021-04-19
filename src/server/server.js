effe55f28cbe6e2d5d55f1f392689c185bcb3193
kaklin <filip.kaklin@gmail.com>
2021-04-19 22:18:26 +0100

    fix build for heroku

--- src/server/server.js
+++ src/server/server.js
@@ -19,3 +19,4 @@ if (process.env.NODE_ENV === 'development') {
 // Listen on port
 const port = process.env.PORT || 5000;
 const server = app.listen(port);
+console.log(`Server listening on port ${port}`);
